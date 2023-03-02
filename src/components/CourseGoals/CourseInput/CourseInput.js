cb3346abebd123e5412162f499c8c56e0c9b9b68
ayreneYS <ayreneyebes@gmail.com>
2023-03-02 16:15:17 +0800

    Project 1

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,26 +5,26 @@ import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const FormControl = styled.div`
-	.form-control {
-		margin: 0.5rem 0;
-	}
+	margin: 0.5rem 0;
 
-	.form-control label {
+	& label {
 		font-weight: bold;
 		display: block;
 		margin-bottom: 0.5rem;
+		color: ${(props) => (props.invalid ? 'red' : 'black')};
 	}
 
-	.form-control input {
+	& input {
 		display: block;
 		width: 100%;
-		border: 1px solid #ccc;
+		border: 1px solid ${(props) => (props.invalid ? 'red' : '#ccc')};
+		background: ${(props) => (props.invalid ? '#ffd7d7' : 'transparent')};
 		font: inherit;
 		line-height: 1.5rem;
 		padding: 0 0.25rem;
 	}
 
-	.form-control input:focus {
+	& input:focus {
 		outline: none;
 		background: #fad0ec;
 		border-color: #8b005d;
@@ -53,10 +53,10 @@ const CourseInput = (props) => {
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+			<FormControl invalid={!isValid}>
 				<label>Course Goal</label>
 				<input type='text' onChange={goalInputChangeHandler} />
-			</div>
+			</FormControl>
 			<Button type='submit'>Add Goal</Button>
 		</form>
 	);
